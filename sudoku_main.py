2d1667efd3553a1f8ab611d1509b7be6fb2afc4e
comeeasy <zz1236zz@naver.com>
2020-12-30 23:43:34 +0900

    Sudoku upload

---
+++ sudoku_main.py
@@ -0,0 +1,20 @@
+from sudoku import *
+
+T = int(input())
+for testCase in range(1, T+1) :
+    # sdk 입력, 빈 list 인자로 줌
+    sdk = sudoku([])
+    for row in range(N) :
+        ithRow = list(map(int, list(input())))
+        # 스도쿠에 loc들을 삽입
+        sdk.matrix.append([loc(row, column, ithRow[column]) for column in range(N)])
+
+    print('=== testCase {0} ==='.format(testCase))
+    print('---------- before -------------')
+    sdk.print()
+
+    sdk = play_sdk(sdk)
+
+    print('\n---------- after -------------')
+    sdk.print()
+
